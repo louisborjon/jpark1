1483e49d76ee631b736a4b52a4c09571a85005d7
Josh_Date <jjdate@live.com>
2019-04-15 14:27:45 -0400

    added new files and signup page

--- backend/jpark/views.py
+++ backend/jpark/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
+from django.http import HttpResponse
 
 # Create your views here.
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            redirect('')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form':form})
