cec837851a6878392d9f2f7ef05d6327cc6a7d3c
Josh_Date <jjdate@live.com>
2019-04-15 21:22:06 -0400

    added changes to templates and forms

--- backend/jpark/forms.py
+++ backend/jpark/forms.py
@@ -1,6 +1,27 @@
 from django.forms import CharField, PasswordInput, Form
 from django.shortcuts import render
+from django import forms
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.models import User
 
 class LoginForm(Form):
     username = CharField(label="User Name", max_length=64)
     password = CharField(widget=PasswordInput())
+
+
+class SignupForm(UserCreationForm):
+    first_name = forms.CharField(max_length=255)
+    last_name = forms.CharField(max_length=255)
+    email = forms.EmailField(max_length=255)
+
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'password1',
+            'password2',
+            )
