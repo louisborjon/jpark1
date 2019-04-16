aa7f242593430a48819f76df94e45720e76adf58
Jose Borjon <jluisborjon@gmail.com>
2019-04-16 16:19:38 -0400

    working on models

--- backend/jpark/models.py
+++ backend/jpark/models.py
@@ -6,7 +6,7 @@ class Profile(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     licence_plate = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
     phone = models.CharField(max_length=255, null=True)
     user = models.OneToOneField(User, related_name='profile', on_delete=models.CASCADE)
     balance = models.CharField(max_length=255)
@@ -24,32 +24,65 @@ class Profile(models.Model):
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=255)
+    """ Category respresenting zone price """
+    zone_1 = 'Z1'
+    zone_2 = 'Z2'
+    zone_3 = 'Z3'
+    zone_in_category_choices = (
+        (zone_1, 'zone_1'),
+        (zone_2, 'zone_2'),
+        (zone_3, 'zone_3'),
+    )
+    zone_price = models.CharField(max_length=2, choices=zone_in_category_choices, default=zone_1)
 
     def __str__(self):
-        return self.name
+        return self.zone_price
 
     def location(self):
-        return self.parking_lots.all()[0].location
+        return self.parking_lots.all()[0].hourly_rate
 
 class Parking(models.Model):
+    CHOICES_IN_SCHOOL_CHOICES = (
+        ('ST', 'Street'),
+        ('AVE', 'Avenue'),
+        ('BLVD', 'Boulevard'),
+    );
+    PROVINCE_CHOICES = (
+        ('BC', 'British Columbia'),
+        ('MB', 'Manitoba'),
+        ('NB', 'New Brunswick'),
+        ('NL', 'Newfoundland and Labrador'),
+        ('NT', 'Northwest Territories'),
+        ('NS', 'Nova Scotia'),
+        ('NU', 'Nunavut'),
+        ('ON', 'Ontario'),
+        ('PE', 'Prince Edward Island'),
+        ('QC', 'Québec'),
+        ('SK', 'Saskatchewan'),
+        ('YT', 'Yukon'),
+    );
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owned_parking_lots')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='parking_lots')
-    guests = models.ManyToManyField(User, through='Reservation', related_name='reserved_parking_lots')
-    name = models.CharField(max_length=255)
-    address = models.CharField(max_length=255)
+    drivers = models.ManyToManyField(User, through='Reservation', related_name='reserved_parking_lots')
+    street_type = models.CharField(max_length=4, choices=PROVINCE_CHOICES, default=British Columbia)
+    street_name = models.CharField(max_length=255)
+    street_number = models.CharField(max_length=255)
+    zip_code = models.CharField(max_length=6)
+    province = models.CharField(max_length=255, choices=choices_in_street_types, default=street)
+    city = models.CharField(max_length=255)
+    image = models.URLField(max_length=255, null=True)
     phone = models.CharField(max_length=255)
-    location = models.URLField(max_length=255, null=True)
     description = models.TextField(null=True)
+    reservation_any_time = ToogleField()
     opening_time = models.TimeField()
     closing_time = models.TimeField()
-    price = models.CharField(max_length=255)
+    hourly_rate = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.name
-
-    def parking_past_midnight(self):
-        return self.closing_time < self.opening_time
+        return self.phone
+    # configurate starting_time, starting_date booth ending
+    # def parking_past_midnight(self):
+    #     return self.closing_time < self.opening_time
 
     def time_frame(self, date, starting_time, ending_time):
         reserved_parking = self.reservations.filter(date=date, starting_time=starting_time, ending_time=ending_time).aggregate(Sum('reserved_time'))
@@ -57,17 +90,20 @@ class Parking(models.Model):
         return (reserved_parking + starting_time) <= self.availability
 
 class Reservation(models.Model):
+    # for reservationes starting_date, ending_date, starting_time, ending_time, =total reservation time
     user = models.ForeignKey(User, related_name='reservations_made', on_delete=models.CASCADE)
     parking = models.ForeignKey(Parking, related_name='reservations', on_delete=models.CASCADE)
-    date = models.DateField()
+    checkout_same_day = ToogleField()
+    starting_date = models.DateField()
+    ending_date = models.DateField()
     starting_time = models.TimeField()
-    reserved_time = models.IntegerField()
+    ending_time = models.TimeField()
+    total_time = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     notes = models.TextField(blank=True)
 
-
     def __str__(self):
-        return "{} {} - Reserved Time {}".format(self.date, self.time, self.reserved_time)
+        return "{} {} - Total Time {}".format(self.date, self.time, self.reserved_time)
 
     def date_and_time(self):
         date = self.date.strftime("%Y-%m-%d")
