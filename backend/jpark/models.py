33e062ac336d7211851f6b7362db816b2dc7c4e1
Jose Borjon <jluisborjon@gmail.com>
2019-04-17 13:17:41 -0400

    models #2

--- backend/jpark/models.py
+++ backend/jpark/models.py
@@ -5,11 +5,11 @@ from django.db.models import Sum
 class Profile(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    licence_plate = models.CharField(max_length=255)
+    licence_plate = models.CharField(max_length=255, null=True)
     email = models.EmailField(max_length=255)
     phone = models.CharField(max_length=255, null=True)
     user = models.OneToOneField(User, related_name='profile', on_delete=models.CASCADE)
-    balance = models.CharField(max_length=255)
+    balance = models.IntegerField(null=True)
 
 
     def __str__(self):
@@ -25,15 +25,19 @@ class Profile(models.Model):
 
 class Category(models.Model):
     """ Category respresenting zone price """
-    zone_1 = 'Z1'
-    zone_2 = 'Z2'
-    zone_3 = 'Z3'
     zone_in_category_choices = (
-        (zone_1, 'zone_1'),
-        (zone_2, 'zone_2'),
-        (zone_3, 'zone_3'),
-    )
-    zone_price = models.CharField(max_length=2, choices=zone_in_category_choices, default=zone_1)
+        ('zone_1', 'Z1'),
+        ('zone_2', 'Z2'),
+        ('zone_3', 'Z3'),
+    );
+    zone_pricing = (
+        ('zone_1', '5'),
+        ('zone_2', '3'),
+        ('zone_3', '1'),
+    );
+    zone_name = models.CharField(max_length=2, choices=zone_in_category_choices, default='zone_1')
+    zone_price = models.IntegerField(choices=zone_pricing)
+
 
     def __str__(self):
         return self.zone_price
@@ -46,6 +50,7 @@ class Parking(models.Model):
         ('ST', 'Street'),
         ('AVE', 'Avenue'),
         ('BLVD', 'Boulevard'),
+        ('RD', 'Road'),
     );
     PROVINCE_CHOICES = (
         ('BC', 'British Columbia'),
@@ -64,29 +69,27 @@ class Parking(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owned_parking_lots')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='parking_lots')
     drivers = models.ManyToManyField(User, through='Reservation', related_name='reserved_parking_lots')
-    street_type = models.CharField(max_length=4, choices=PROVINCE_CHOICES, default='BC')
+    street_type = models.CharField(max_length=4, choices=CHOICES_IN_STREET_TYPES, default='ST')
     street_name = models.CharField(max_length=255)
-    street_number = models.CharField(max_length=255)
+    street_number = models.IntegerField()
     zip_code = models.CharField(max_length=6)
-    province = models.CharField(max_length=255, choices=CHOICES_IN_STREET_TYPES, default='ST')
+    province = models.CharField(max_length=255, choices=PROVINCE_CHOICES, default='ON')
     city = models.CharField(max_length=255)
     image = models.URLField(max_length=255, null=True)
-    phone = models.CharField(max_length=255)
+    phone = models.IntegerField()
     description = models.TextField(null=True)
-    # reservation_any_time = ToggleField()
     opening_time = models.TimeField()
     closing_time = models.TimeField()
-    hourly_rate = models.CharField(max_length=255)
+    hourly_rate = models.IntegerField()
 
     def __str__(self):
-        return self.phone
-    # configurate starting_time, starting_date booth ending
+        return "Your parking spot is located at {} {} {}".format(self.street_number, self.street_name, self.street_type)
     # def parking_past_midnight(self):
     #     return self.closing_time < self.opening_time
 
     def time_frame(self, date, starting_time, ending_time):
         reserved_parking = self.reservations.filter(date=date, starting_time=starting_time, ending_time=ending_time).aggregate(Sum('reserved_time'))
-        reserved_parking = reserved_parking['reserved_time__sum'] or 0
+        reserved_parking = reserved_parking['reserved_time__sum'] or 0 #trying to not allow two reservations overlapping
         return (reserved_parking + starting_time) <= self.availability
 
 class Reservation(models.Model):
@@ -98,12 +101,11 @@ class Reservation(models.Model):
     ending_date = models.DateField()
     starting_time = models.TimeField()
     ending_time = models.TimeField()
-    total_time = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     notes = models.TextField(blank=True)
 
     def __str__(self):
-        return "{} {} - Total Time {}".format(self.date, self.time, self.reserved_time)
+        return "Your reservation is on {} - Total Time {}".format(self.starting_date, (self.ending_time - self.starting_time).hours)
 
     def date_and_time(self):
         date = self.date.strftime("%Y-%m-%d")
