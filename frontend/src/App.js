1a526dbfd828b5e7b31ef37a11e13fd3c895ec3c
Jose Borjon <jluisborjon@gmail.com>
2019-04-19 00:11:41 -0400

    react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 class App extends Component {
+  state = {
+    profiles:[]
+  };
+  async componentDidMount() {
+    try {
+      const res = await fetch('http://127.0.0.1:8000/api/');
+      const profiles = await res.json();
+      this.setState({
+        profiles
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        {this.state.profiles.map(item => (
+          <div key={item.id}>
+            <h1>{item.title}</h1>
+            <span>{item.description}</span>
+          </div>
+        ))}
       </div>
     );
   }
